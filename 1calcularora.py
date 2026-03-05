ac838958515ab94dc2af82cf7bead904bc4fd3f0
Antonella216yt <antonellaauqui@gmail.com>
2026-03-05 14:47:16 -0500

    lo que le estoy aumemtando

---
+++ 1calcularora.py
@@ -0,0 +1,10 @@
+pedir1=int (input('dame numero 1;'))
+pedir2=int (input('dame numero 2;'))
+a= input ("ingrese la operacion ")
+if a =="+":
+    sumar  = pedir1 + pedir2 
+    print ("el resultado es",sumar)
+
+elif a == "-":
+    restar= pedir1-pedir2
+    print("el resultado es",restar)
