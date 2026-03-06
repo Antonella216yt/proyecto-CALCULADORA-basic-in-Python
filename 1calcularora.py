cdbbee45ac51f233896fb01a8005d89912253f19
Antonella216yt <antonellaauqui@gmail.com>
2026-03-05 19:22:59 -0500

    codigo mas corto

--- 1calcularora.py
+++ 1calcularora.py
@@ -3,8 +3,23 @@ pedir2=int (input('dame numero 2;'))
 a= input ("ingrese la operacion ")
 if a =="+":
     sumar  = pedir1 + pedir2 
-    print ("el resultado es",sumar)
+    print ("el resultado es",sumar) 
+
 
 elif a == "-":
     restar= pedir1-pedir2
     print("el resultado es",restar)
+
+
+elif a == "*": 
+    multiplicar= pedir1*pedir2
+    print("el resultado es",multiplicar)
+
+elif a =="/":
+    division = pedir1/pedir2
+    print("el resultado es",division)
+
+elif a== "%":
+    modulo = pedir1%pedir2
+    print("el resultado es",modulo)
+
---
+++ 2calculadoraww.py
@@ -0,0 +1,18 @@
+pedir1=int (input('dame numero 1;'))
+pedir2=int (input('dame numero 2;'))
+a= input ("ingrese la operacion ")
+
+if a =='+':
+    print("es",pedir1+pedir2)
+
+elif a=='-':
+    print('es',pedir1-pedir2)
+
+elif a =='*':
+    print('es',pedir1-pedir2)
+
+elif a =='/':
+    print('es',pedir1/pedir2)
+
+elif a=="%":
+    print("es",pedir1%pedir2)
